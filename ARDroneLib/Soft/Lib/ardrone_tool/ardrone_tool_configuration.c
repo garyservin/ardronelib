9579515f383a0af384d92d56b357ff7d616422cd
Gary Servin <garyservin@gmail.com>
2014-10-07 13:31:59 -0300

    revert

--- ARDroneLib/Soft/Lib/ardrone_tool/ardrone_tool_configuration.c
+++ ARDroneLib/Soft/Lib/ardrone_tool/ardrone_tool_configuration.c
@@ -138,11 +138,9 @@ bool_t ardrone_tool_configuration_addevent_##NAME(C_TYPE_PTR value, ardrone_tool
     blog3(" strlen((char*)value) + 1 = %d\n", strlen((char*)value) + 1); \
     blog3(" size = %d\n", ((strlen((char*)value) + 1) * sizeof(C_TYPE))); \
     blog3(" value = %s\n", value); \
-    blog3("size of value = %d\n", sizeof(*value)); \
-    blog3("size of array value = %d\n", sizeof(*value)/sizeof(*value[0])); \
     blog3("size of dest value = %d\n", sizeof(*ardrone_tool_configuration_data[ardrone_tool_configuration_nb_event].value)); \
     blog3("add * to szeofs \n"); \
-	*ardrone_tool_configuration_data[ardrone_tool_configuration_nb_event].value = *value;		\
+			vp_os_memcpy(ardrone_tool_configuration_data[ardrone_tool_configuration_nb_event].value, value, (strlen((char*)value) + 1) * sizeof(C_TYPE));		\
     }\
     blog3("25\n"); \
 			ardrone_tool_configuration_data[ardrone_tool_configuration_nb_event].callback	= (ardrone_at_configuration_set)&ARDRONE_CONFIGURATION_SET_FUNCTION(NAME);	\
